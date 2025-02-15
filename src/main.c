d7694b70f5ed1311aa13a09acd4a1afd51048dad
Nathan Walker <nathanwalker1208@gmail.com>
2025-02-15 10:20:12 -0700

    Define registers

--- src/main.c
+++ src/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+struct Registers {
+  // System registers
+  unsigned short ip; // Instruction pointer
+  unsigned short sp; // Stack pointer
+
+  // General registers
+  unsigned int a; // eax, ax, ah, al
+  unsigned int b; // ebx, bx, bh, bl
+  unsigned int c; // ecx, cx, ch, cl
+  unsigned int d; // edx, dx, dh, dl
+};
+
 int main() {
   printf("Hello, from Embattled!\n");
 }
