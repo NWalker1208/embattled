b935be919c5c0ba6d43a6c097137dfaef055cd7b
Nathan Walker <nathanwalker1208@gmail.com>
2025-02-17 09:51:30 -0700

    Define values for opcodes

--- include/instruction.h
+++ include/instruction.h
@@ -1,51 +1,52 @@
-// Layout (indicated by last 3 bits of opcode):
+// Layout:
 // 0: | opcode (8 bits) |
 // 1: | opcode (8 bits) | reg A (4 bits) | reg B (4 bits) |
 // 2: | opcode (8 bits) | reg A (4 bits) | imm (4 bits)   |
 // 3: | opcode (8 bits) | imm (8 bits)                    |
 // 4: | opcode (8 bits) | imm (16 bits)                                                     |
+// (last 2 bits of opcode represent number of bytes in addition to opcode)
 
 enum Opcode {
   // Control flow
-  NOP,  // layout 0 | no params
-  JMP,  // layout 4 | imm[16]
-  JMZ,  // layout 4 | imm[16]
-  JML,  // layout 4 | imm[16]
+  NOP  = 0b000000'00, // layout 0 | no params
+  JMP  = 0b000001'10, // layout 4 | imm[16]
+  JMZ  = 0b000010'10, // layout 4 | imm[16]
+  JML  = 0b000011'10, // layout 4 | imm[16]
   // Memory
-  MOV,  // layout 1 | reg, reg
-  LDIB, // layout 3 | imm[8]
-  LDIW, // layout 4 | imm[16]
-  LDMB, // layout 1 | reg, reg
-  LDMW, // layout 1 | reg, reg
-  STMB, // layout 1 | reg, reg
-  STMW, // layout 1 | reg, reg
-  PSHB, // layout 0 | no params
-  PSHW, // layout 0 | no params
-  POPB, // layout 0 | no params
-  POPW, // layout 0 | no params
+  MOV  = 0b000100'01, // layout 1 | reg, reg
+  LDIB = 0b000110'01, // layout 3 | imm[8]
+  LDIW = 0b000111'10, // layout 4 | imm[16]
+  LDMB = 0b001000'01, // layout 1 | reg, reg
+  LDMW = 0b001001'01, // layout 1 | reg, reg
+  STMB = 0b001010'01, // layout 1 | reg, reg
+  STMW = 0b001011'01, // layout 1 | reg, reg
+  PSHB = 0b001100'00, // layout 0 | no params
+  PSHW = 0b001101'00, // layout 0 | no params
+  POPB = 0b001110'00, // layout 0 | no params
+  POPW = 0b001111'00, // layout 0 | no params
   // Math and logic
-  ADD,  // layout 1 | reg, reg
-  SUB,  // layout 1 | reg, reg
-  MUL,  // layout 1 | reg, reg
-  DIVS, // layout 1 | reg, reg
-  DIVU, // layout 1 | reg, reg
-  REMS, // layout 1 | reg, reg
-  REMU, // layout 1 | reg, reg
-  LSH,  // layout 1 | reg, reg
-  RSHS, // layout 1 | reg, reg
-  RSHU, // layout 1 | reg, reg
-  LSI,  // layout 2 | reg, imm[4]
-  RSIS, // layout 2 | reg, imm[4]
-  RSIU, // layout 2 | reg, imm[4]
-  AND,  // layout 1 | reg, reg
-  IOR,  // layout 1 | reg, reg
-  XOR,  // layout 1 | reg, reg
-  CEQ,  // layout 1 | reg, reg
-  CNE,  // layout 1 | reg, reg
-  CLTS, // layout 1 | reg, reg
-  CLTU, // layout 1 | reg, reg
-  CGES, // layout 1 | reg, reg
-  CGEU, // layout 1 | reg, reg
+  ADD  = 0b010000'01, // layout 1 | reg, reg
+  SUB  = 0b010001'01, // layout 1 | reg, reg
+  MUL  = 0b010010'01, // layout 1 | reg, reg
+  DIVS = 0b010110'01, // layout 1 | reg, reg
+  DIVU = 0b010111'01, // layout 1 | reg, reg
+  REMS = 0b011000'01, // layout 1 | reg, reg
+  REMU = 0b011001'01, // layout 1 | reg, reg
+  LSH  = 0b011010'01, // layout 1 | reg, reg
+  RSHS = 0b011011'01, // layout 1 | reg, reg
+  RSHU = 0b011100'01, // layout 1 | reg, reg
+  LSI  = 0b011101'01, // layout 2 | reg, imm[4]
+  RSIS = 0b011110'01, // layout 2 | reg, imm[4]
+  RSIU = 0b011111'01, // layout 2 | reg, imm[4]
+  AND  = 0b100000'01, // layout 1 | reg, reg
+  IOR  = 0b100001'01, // layout 1 | reg, reg
+  XOR  = 0b100010'01, // layout 1 | reg, reg
+  CEQ  = 0b100011'01, // layout 1 | reg, reg
+  CNE  = 0b100100'01, // layout 1 | reg, reg
+  CLTS = 0b100101'01, // layout 1 | reg, reg
+  CLTU = 0b100110'01, // layout 1 | reg, reg
+  CGES = 0b100111'01, // layout 1 | reg, reg
+  CGEU = 0b101000'01, // layout 1 | reg, reg
 };
 
 enum Register {
