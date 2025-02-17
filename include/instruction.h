27c0183b2d515f614d11c63188a172bf53b7ab6d
Nathan Walker <nathanwalker1208@gmail.com>
2025-02-17 09:48:36 -0700

    Redefine layouts to be more intuitive

--- include/instruction.h
+++ include/instruction.h
@@ -1,36 +1,57 @@
+// Layout (indicated by last 3 bits of opcode):
+// 0: | opcode (8 bits) |
+// 1: | opcode (8 bits) | reg A (4 bits) | reg B (4 bits) |
+// 2: | opcode (8 bits) | reg A (4 bits) | imm (4 bits)   |
+// 3: | opcode (8 bits) | imm (8 bits)                    |
+// 4: | opcode (8 bits) | imm (16 bits)                                                     |
+
 enum Opcode {
   // Control flow
-  NOP,
-  JMP,
-  JMZ,
+  NOP,  // layout 0 | no params
+  JMP,  // layout 4 | imm[16]
+  JMZ,  // layout 4 | imm[16]
+  JML,  // layout 4 | imm[16]
   // Memory
-  MOV,
-  LDI,
-  LDM,
-  STM,
+  MOV,  // layout 1 | reg, reg
+  LDIB, // layout 3 | imm[8]
+  LDIW, // layout 4 | imm[16]
+  LDMB, // layout 1 | reg, reg
+  LDMW, // layout 1 | reg, reg
+  STMB, // layout 1 | reg, reg
+  STMW, // layout 1 | reg, reg
+  PSHB, // layout 0 | no params
+  PSHW, // layout 0 | no params
+  POPB, // layout 0 | no params
+  POPW, // layout 0 | no params
   // Math and logic
-  ADD,
-  SUB,
-  MUL,
-  DIVS,
-  DIVU,
-  REMS,
-  REMU,
-  LSH,
-  RSHS,
-  RSHU,
-  AND,
-  IOR,
-  XOR,
-  CEQ,
-  CNE,
-  CLTS,
-  CLTU,
-  CGES,
-  CGEU,
+  ADD,  // layout 1 | reg, reg
+  SUB,  // layout 1 | reg, reg
+  MUL,  // layout 1 | reg, reg
+  DIVS, // layout 1 | reg, reg
+  DIVU, // layout 1 | reg, reg
+  REMS, // layout 1 | reg, reg
+  REMU, // layout 1 | reg, reg
+  LSH,  // layout 1 | reg, reg
+  RSHS, // layout 1 | reg, reg
+  RSHU, // layout 1 | reg, reg
+  LSI,  // layout 2 | reg, imm[4]
+  RSIS, // layout 2 | reg, imm[4]
+  RSIU, // layout 2 | reg, imm[4]
+  AND,  // layout 1 | reg, reg
+  IOR,  // layout 1 | reg, reg
+  XOR,  // layout 1 | reg, reg
+  CEQ,  // layout 1 | reg, reg
+  CNE,  // layout 1 | reg, reg
+  CLTS, // layout 1 | reg, reg
+  CLTU, // layout 1 | reg, reg
+  CGES, // layout 1 | reg, reg
+  CGEU, // layout 1 | reg, reg
 };
 
 enum Register {
+  IP,
+  SP,
+  AC,
   X0 = 0,
   X1 = 1,
   X2 = 2,
