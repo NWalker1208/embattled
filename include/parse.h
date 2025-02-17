abf08ed2dca4f11563aeebb55fa41972a185d865
Nathan Walker <nathanwalker1208@gmail.com>
2025-02-17 10:23:08 -0700

    Add documentation to parseNextInstruction

--- include/parse.h
+++ include/parse.h
@@ -1,3 +1,5 @@
 #include "instruction.h"
 
+// Parses the next instruction from the given text and advances the text pointer.
+// If an invalid instruction is encountered, the text pointer is set to NULL.
 struct Instruction parseNextInstruction(char** text);
