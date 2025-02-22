a28502da3e75d44ab0f8b811fceda0893ea57f0e
Nathan Walker <nathanwalker1208@gmail.com>
2025-02-21 18:39:58 -0700

    Implement file loading

--- src/main.c
+++ src/main.c
@@ -4,28 +4,41 @@
 #include "process.h"
 #include "instruction.h"
 
-int main() {
-  printf("Hello, from Embattled!\n");
+bool tryParseInt(const char* str, int* result);
 
+int main(int argc, char* argv[]) {
+  // Parse arguments (file name of binary, number of steps to run)
+  if (argc != 3) {
+    printf("Usage: %s <binary file> <number of steps>\n", argv[0]);
+    return 1;
+  }
+
+  char* binaryFileName = argv[1];
+  long numberOfSteps;
+  if (!tryParseInt(argv[2], &numberOfSteps)) {
+    printf("Invalid number of steps: %s\n", argv[2]);
+    return 1;
+  }
+
+  // Open binary file
+  FILE* binaryFile = fopen(binaryFileName, "rb");
+  if (binaryFile == NULL) {
+    printf("Failed to open binary file: %s\n", binaryFileName);
+    return 1;
+  }
+
+  // Read first MEMORY_SIZE bytes from binary file
   unsigned char memory[MEMORY_SIZE] = { 0 };
+  size_t bytesRead = fread(memory, sizeof(unsigned char), MEMORY_SIZE, binaryFile);
+  if (bytesRead == 0) {
+    printf("Failed to read binary file: %s\n", binaryFileName);
+    fclose(binaryFile);
+    return 1;
+  }
+  fclose(binaryFile);
+
+  // Run process for numberOfSteps steps
   struct ProcessState processState = { 0 };
-
-  memory[0] = OPCODE_VALUES[LDIW];
-  memory[1] = 0x01;
-  memory[2] = 0x00;
-  
-  memory[3] = OPCODE_VALUES[MOV];
-  memory[4] = REGISTER_IDS[X0] << 4 | REGISTER_IDS[AC];
-  
-  memory[5] = OPCODE_VALUES[ADD];
-  memory[6] = REGISTER_IDS[AC] << 4 | REGISTER_IDS[X0];
-
-  memory[7] = OPCODE_VALUES[PSHW];
-  memory[8] = REGISTER_IDS[AC] << 4;
-
-  memory[9] = OPCODE_VALUES[JMP];
-  memory[10] = 0x05;
-  memory[11] = 0x00;
 
   while (1) {
     stepProcess(memory, &processState);
@@ -49,3 +62,23 @@ bool tryParseInt(const char* str, int* result) {
   *result = (int)value;
   return true;
 }
+
+/*
+Example program:
+  memory[0] = OPCODE_VALUES[LDIW];
+  memory[1] = 0x01;
+  memory[2] = 0x00;
+  
+  memory[3] = OPCODE_VALUES[MOV];
+  memory[4] = REGISTER_IDS[X0] << 4 | REGISTER_IDS[AC];
+  
+  memory[5] = OPCODE_VALUES[ADD];
+  memory[6] = REGISTER_IDS[AC] << 4 | REGISTER_IDS[X0];
+
+  memory[7] = OPCODE_VALUES[PSHW];
+  memory[8] = REGISTER_IDS[AC] << 4;
+
+  memory[9] = OPCODE_VALUES[JMP];
+  memory[10] = 0x05;
+  memory[11] = 0x00;
+*/
