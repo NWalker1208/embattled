b2ad7f41e4479ccd19426c307e2ebb99fbd36948
Nathan Walker <nathanwalker1208@gmail.com>
2025-02-22 12:19:21 -0700

    Define a Registers struct

--- processor/include/processor/register.h
+++ processor/include/processor/register.h
@@ -19,6 +19,26 @@ enum Register {
   X11,
 };
 
+struct Registers {
+  // Special registers
+  unsigned short ip;
+  unsigned short sp;
+  unsigned short ac;
+  // General registers
+  unsigned short x0;
+  unsigned short x1;
+  unsigned short x2;
+  unsigned short x3;
+  unsigned short x4;
+  unsigned short x5;
+  unsigned short x6;
+  unsigned short x7;
+  unsigned short x8;
+  unsigned short x9;
+  unsigned short x10;
+  unsigned short x11;
+};
+
 extern const unsigned int REGISTER_COUNT;
 extern const unsigned char REGISTER_IDS[];
 extern const char* REGISTER_NAMES[];
