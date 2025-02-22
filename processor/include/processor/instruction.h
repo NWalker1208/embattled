10a75a3cd0e750503deba54dc9c5b5ee901d1e01
Nathan Walker <nathanwalker1208@gmail.com>
2025-02-22 12:16:06 -0700

    Split opcode definitions into separate source and header files

--- processor/include/processor/instruction.h
+++ processor/include/processor/instruction.h
@@ -6,55 +6,6 @@
 // 4: | opcode (8 bits) | imm (16 bits)                                                     |
 // (last 2 bits of opcode represent number of bytes in addition to opcode)
 
-enum Opcode {
-  // Control flow
-  NOP,  // layout 0 | no params   | no effect
-  JMP,  // layout 4 | imm[16]     | ac = ip; ip = imm
-  JMZ,  // layout 4 | imm[16]     | if ac == 0, then ip = imm, else no effect
-  SLP,  // layout 2 | reg, imm[4] | sleep for reg + (unsigned) imm cycles
-  // Memory
-  MOV,  // layout 1 | reg, reg    | regA = regB
-  LDIB, // layout 3 | imm[8]      | ac = 0:imm
-  LDIW, // layout 4 | imm[16]     | ac = imm
-  LDMB, // layout 2 | reg, imm[4] | ac = 0:mem[addr]             where addr = regA + imm (signed)
-  LDMW, // layout 2 | reg, imm[4] | ac = mem[addr + 1]:mem[addr] where addr = regA + imm (signed)
-  STMB, // layout 2 | reg, imm[4] | mem[addr] = ac (low)         where addr = regA + imm (signed)
-  STMW, // layout 2 | reg, imm[4] | mem[addr + 1]:mem[addr] = ac where addr = regA + imm (signed)
-  PSHB, // layout 2 | reg         | mem[sp - 1] = regA (low); sp -= 1
-  PSHW, // layout 2 | reg         | mem[sp - 1]:mem[sp - 2] = regA; sp -= 2
-  POPB, // layout 2 | reg         | sp += 1; regA = 0:mem[sp - 1]
-  POPW, // layout 2 | reg         | sp += 2; regA = mem[sp - 1]:mem[sp - 2]; 
-  // Math and logic
-  INC,  // layout 2 | reg, imm[4] | regA += imm (unsigned)
-  DEC,  // layout 2 | reg, imm[4] | regA -= imm (unsigned)
-  ADD,  // layout 1 | reg, reg    | ac = regA + regB
-  SUB,  // layout 1 | reg, reg    | ac = regA - regB
-  MUL,  // layout 1 | reg, reg    | ac = regA * regB
-  DIVS, // layout 1 | reg, reg    | ac = regA (signed)   / regB (signed)
-  DIVU, // layout 1 | reg, reg    | ac = regA (unsigned) / regB (unsigned)
-  REMS, // layout 1 | reg, reg    | ac = regA (signed)   % regB (signed)
-  REMU, // layout 1 | reg, reg    | ac = regA (unsigned) % regB (unsigned)
-  LSH,  // layout 1 | reg, reg    | ac = regA << regB (signed)
-  RSHS, // layout 1 | reg, reg    | ac = regA (signed)   >> regB (signed)
-  RSHU, // layout 1 | reg, reg    | ac = regA (unsigned) >> regB (signed)
-  LSI,  // layout 2 | reg, imm[4] | ac = regA << imm (unsigned)
-  RSIS, // layout 2 | reg, imm[4] | ac = regA (signed)   >> imm (unsigned)
-  RSIU, // layout 2 | reg, imm[4] | ac = regA (unsigned) >> imm (unsigned)
-  AND,  // layout 1 | reg, reg    | ac = regA & regB
-  IOR,  // layout 1 | reg, reg    | ac = regA | regB
-  XOR,  // layout 1 | reg, reg    | ac = regA ^ regB
-  CEQ,  // layout 1 | reg, reg    | ac = (regA == regB) ? 1 : 0
-  CNE,  // layout 1 | reg, reg    | ac = (regA != regB) ? 1 : 0
-  CLTS, // layout 1 | reg, reg    | ac = (regA (signed) < regB (signed)) ? 1 : 0
-  CLTU, // layout 1 | reg, reg    | ac = (regA (unsigned) < regB (unsigned)) ? 1 : 0
-  CGES, // layout 1 | reg, reg    | ac = (regA (signed) >= regB (signed)) ? 1 : 0
-  CGEU, // layout 1 | reg, reg    | ac = (regA (unsigned) >= regB (unsigned)) ? 1 : 0
-};
-
-extern const unsigned int OPCODE_COUNT;
-extern const unsigned char OPCODE_VALUES[];
-extern const char* OPCODE_NAMES[];
-
 enum Register {
   // Special registers
   NL, // Null register. Always 0.
--- processor/src/instruction.c
+++ processor/src/instruction.c
@@ -1,97 +1,5 @@
 #include "processor/instruction.h"
 
-const unsigned int OPCODE_COUNT = 36;
-
-const unsigned char OPCODE_VALUES[] = {
-  // Control flow
-  0b000000'00, // NOP
-  0b000001'10, // JMP
-  0b000010'10, // JMZ
-  0b000011'01, // SLP
-  // Memory
-  0b000100'01, // MOV
-  0b000110'01, // LDIB
-  0b000111'10, // LDIW
-  0b001000'01, // LDMB
-  0b001001'01, // LDMW
-  0b001010'01, // STMB
-  0b001011'01, // STMW
-  0b001100'01, // PSHB
-  0b001101'01, // PSHW
-  0b001110'01, // POPB
-  0b001111'01, // POPW
-  // Math and logic
-  0b010000'01, // INC
-  0b010001'01, // DEC
-  0b010010'01, // ADD
-  0b010011'01, // SUB
-  0b010100'01, // MUL
-  0b011000'01, // DIVS
-  0b011001'01, // DIVU
-  0b011010'01, // REMS
-  0b011011'01, // REMU
-  0b011100'01, // LSH
-  0b011101'01, // RSHS
-  0b011110'01, // RSHU
-  0b011111'01, // LSI
-  0b100000'01, // RSIS
-  0b100001'01, // RSIU
-  0b100010'01, // AND
-  0b100011'01, // IOR
-  0b100100'01, // XOR
-  0b100101'01, // CEQ
-  0b100110'01, // CNE
-  0b100111'01, // CLTS
-  0b101000'01, // CLTU
-  0b101001'01, // CGES
-  0b101010'01, // CGEU
-};
-
-const char* OPCODE_NAMES[] = {
-  // Control flow
-  "NOP",
-  "JMP",
-  "JMZ",
-  "SLP",
-  // Memory
-  "MOV",
-  "LDIB",
-  "LDIW",
-  "LDMB",
-  "LDMW",
-  "STMB",
-  "STMW",
-  "PSHB",
-  "PSHW",
-  "POPB",
-  "POPW",
-  // Math and logic
-  "INC",
-  "DEC",
-  "ADD",
-  "SUB",
-  "MUL",
-  "DIVS",
-  "DIVU",
-  "REMS",
-  "REMU",
-  "LSH",
-  "RSHS",
-  "RSHU",
-  "LSI",
-  "RSIS",
-  "RSIU",
-  "AND",
-  "IOR",
-  "XOR",
-  "CEQ",
-  "CNE",
-  "CLTS",
-  "CLTU",
-  "CGES",
-  "CGEU",
-};
-
 const unsigned int REGISTER_COUNT = 16;
 
 const unsigned char REGISTER_IDS[] = {
@@ -131,15 +39,6 @@ const char* REGISTER_NAMES[] = {
   "x10",
   "x11",
 };
-
-enum Opcode decodeOpcode(unsigned char opcodeValue) {
-  for (unsigned int i = 0; i < OPCODE_COUNT; i++) {
-    if (opcodeValue == OPCODE_VALUES[i]) {
-      return i;
-    }
-  }
-  return NOP;
-}
 
 enum Register decodeRegister(unsigned char registerId) {
   for (unsigned int i = 0; i < REGISTER_COUNT; i++) {
