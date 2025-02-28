b3dd39615628cb37d4ddc1a710c75fffa248c431
Nathan Walker <nathanwalker1208@gmail.com>
2025-02-27 22:28:22 -0700

    Move union definition out of InstructionParameters

--- processor/include/processor/instruction.h
+++ processor/include/processor/instruction.h
@@ -12,21 +12,24 @@
 // [ opcode (8 bits) | imm[0:7] (8 bits)                       | reg A (4 bits)     | reg B (4 bits)     ]
 // [ opcode (8 bits) | imm[0:15] (16 bits)                                                               | reg A (4 bits)     | reg B (4 bits)     ]
 
+// The value of the immediate parameter encoded in an instruction.
+union ImmediateValue {
+  unsigned short u16 : 16;
+  unsigned short u12 : 12;
+  unsigned short u8 : 8;
+  unsigned short u4 : 4;
+
+  signed short s16 : 16;
+  signed short s12 : 12;
+  signed short s8 : 8;
+  signed short s4 : 4;
+};
+
 // Represents the parameters of an instruction as fetched from memory.
 struct InstructionParameters {
   enum Register registerA : 4;
   enum Register registerB : 4;
-  union {
-    unsigned short u16 : 16;
-    unsigned short u12 : 12;
-    unsigned short u8 : 8;
-    unsigned short u4 : 4;
-
-    signed short s16 : 16;
-    signed short s12 : 12;
-    signed short s8 : 8;
-    signed short s4 : 4;
-  } immediate;
+  union ImmediateValue immediate;
 };
 
 // An instruction that has been fetched from memory.
