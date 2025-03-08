814ccf502b5f58f2e11ae28424caf4c7e920c468
Nathan Walker <nathanwalker1208@gmail.com>
2025-03-07 20:24:50 -0700

    Change how tryParseAssemblyLine handles errors

--- parser/include/parser/parse.h
+++ parser/include/parser/parse.h
@@ -21,9 +21,14 @@
 // Spaces and tabs between any two tokens are ignored.
 // Any whitespace between a label and the subsequent token is ignored.
 
+struct ParsingError {
+  const char* message;
+  const char* location;
+};
+
 // Parses the next line of assembly code from the given text and advances the text pointer.
 // Compatible with both Unix and Windows line endings.
 // Always advances text past the end of the current line.
-// If parsing succeeds, outputs the parsed line through result and returns true.
-// If parsing fails, prints one or more errors to the provided err file and returns false.
-bool tryParseAssemblyLine(FILE* err, const char** text, struct AssemblyLine* result);
+// If parsing succeeds, outputs the parsed line through line and returns true.
+// If parsing fails, outputs the cause through error and returns false.
+bool tryParseAssemblyLine(const char** text, struct AssemblyLine* line, struct ParsingError* error);
--- parser/src/parse.c
+++ parser/src/parse.c
@@ -9,75 +9,73 @@
 // Parses an assembly instruction.
 // If the current line is a valid instruction, advances text to the end of the line,
 // outputs the instruction through result, and returns true.
-// If parsing fails, prints one or more errors to the provided err file,
-// leaves text and result as they are, and returns false.
-bool tryParseInstruction(FILE* err, const char** text, struct AssemblyInstruction* result);
+// If parsing fails, outputs the cause through error and returns false.
+bool tryParseInstruction(const char** text, struct AssemblyInstruction* instruction, struct ParsingError* error);
 
 // Parses hexadecimal assembly data bytes.
 // Expects text to be at the first hexadecimal byte (past the '.data' sequence).
 // If text contains valid hexadecimal bytes from the start to the end of the line,
 // advances text to the end of the line, outputs the data through result, and returns true.
-// If parsing fails, prints one or more errors to the provided err file,
-// leaves text and result as they are, and returns false.
-bool tryParseAssemblyData(FILE* err, const char** text, struct AssemblyData* result);
+// If parsing fails, outputs the cause through error and returns false.
+bool tryParseAssemblyData(const char** text, struct AssemblyData* data, struct ParsingError* error);
 
 // Parses an assembly instruction parameter.
 // If parsing succeeds, advances text past the end of the parameter,
-// outputs the parameter through result and returns true.
-// If parsing fails, leaves text and result as they are and returns false.
-bool tryParseParameter(const char** text, struct AssemblyParameter* result);
+// outputs the parameter through result, and returns true.
+// If parsing fails, returns false.
+bool tryParseParameter(const char** text, struct AssemblyParameter* parameter);
 
 // Parses an opcode name.
 // Expects text to be at the start of the opcode name.
 // If parsing succeeds, advances text past the end of the opcode name,
 // outputs the opcode through result and returns true.
-// If parsing fails, leaves text and result as they are and returns false.
-bool tryParseOpcode(const char** text, enum Opcode* result);
+// If parsing fails, returns false.
+bool tryParseOpcode(const char** text, enum Opcode* opcode);
 
 // Parses a register name.
 // Expects text to be at the start of the register name (past the "$" character).
 // If parsing succeeds, advances text past the end of the register name,
 // outputs the register through result and returns true.
-// If parsing fails, leaves text and result as they are and returns false.
-bool tryParseRegister(const char** text, enum Register* result);
+// If parsing fails, returns false.
+bool tryParseRegister(const char** text, enum Register* reg);
 
 // Parses a hexadecimal immediate value.
 // Expects text to be at the first hexadecimal digit (pase the "0x" sequence).
 // If parsing succeeds, advances text past the end of the hexadecimal digits,
 // outputs the immediate value through result and returns true.
-// If parsing fails, leaves text and result as they are and returns false.
-bool tryParseImmediateHexValue(const char** text, signed int* result);
+// If parsing fails, returns false.
+bool tryParseImmediateHexValue(const char** text, signed int* value);
 
 // Parses a decimal immediate value.
 // Expects text to be at the first digit, or at the sign character if one is present.
 // If parsing succeeds, advances text past the end of the digits,
 // outputs the immediate value through result and returns true.
-// If parsing fails, leaves text and result as they are and returns false.
-bool tryParseImmediateDecValue(const char** text, signed int* result);
+// If parsing fails, returns false.
+bool tryParseImmediateDecValue(const char** text, signed int* value);
 
-// Parses a label name.
+// Parses and copies a label name.
 // If parsing succeeds, advances text past the end of the label and returns a pointer to a new
 // null-terminated array containing a copy of the label.
 // If parsing fails, leaves text as it is and returns NULL.
-char* tryParseLabel(const char** text);
+char* tryCopyLabel(const char** text);
 
 #pragma endregion
 
-bool tryParseAssemblyLine(FILE* err, const char** text, struct AssemblyLine* result) {
-  struct AssemblyLine partialResult = { 0 }; // Copied to result on success
+bool tryParseAssemblyLine(const char** text, struct AssemblyLine* line, struct ParsingError* error) {
+  struct AssemblyLine result = { 0 }; // Copied to result on success
   skipAllWhitespace(text);
 
   // Parse the label if one is present
   if (findCharOnLine(*text, ':') != NULL) {
-    partialResult.label = tryParseLabel(text);
-    if (partialResult.label == NULL) {
-      destroyAssemblyLine(&partialResult);
+    result.label = tryCopyLabel(text);
+    if (result.label == NULL) {
+      destroyAssemblyLine(&result);
       skipToNextLine(text);
       return false; // Failed to parse label
     }
     skipInlineWhitespace(text);
     if (**text != ':') {
-      destroyAssemblyLine(&partialResult);
+      destroyAssemblyLine(&result);
       skipToNextLine(text);
       return false; // Expected ':' after label
     }
@@ -87,16 +85,16 @@ bool tryParseAssemblyLine(FILE* err, const char** text, struct AssemblyLine* res
   // If line starts with ".data", parse as assembly data.
   // Otherwise, parse as an assembly instruction.
   if (startsWithCaseInsensitive(*text, ".data")) {
-    partialResult.kind = DATA;
-    if (!tryParseAssemblyData(err, text, &partialResult.data)) {
-      destroyAssemblyLine(&partialResult);
+    result.kind = DATA;
+    if (!tryParseAssemblyData(text, &result.data, error)) {
+      destroyAssemblyLine(&result);
       skipToNextLine(text);
       return false; // Failed to parse assembly data
     }
   } else {
-    partialResult.kind = INSTRUCTION;
-    if (!tryParseInstruction(err, text, &partialResult.instruction)) {
-      destroyAssemblyLine(&partialResult);
+    result.kind = INSTRUCTION;
+    if (!tryParseInstruction(text, &result.instruction, error)) {
+      destroyAssemblyLine(&result);
       skipToNextLine(text);
       return false; // Failed to parse assembly instruction
     }
@@ -104,13 +102,13 @@ bool tryParseAssemblyLine(FILE* err, const char** text, struct AssemblyLine* res
 
   // Check that we are at the end of the line
   if (**text != '\r' && **text != '\n') {
-    destroyAssemblyLine(&partialResult);
+    destroyAssemblyLine(&result);
     skipToNextLine(text);
     return false; // Expected end of line
   }
 
   // Copy out result, advance to next line, and return success.
-  *result = partialResult;
+  *line = result;
   skipToNextLine(text);
   return true;
 }
