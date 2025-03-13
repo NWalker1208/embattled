e1a4abcc8a16e3d9edcc9fc88f6fce84371616b4
Nathan Walker <nathanwalker1208@gmail.com>
2025-03-12 19:30:54 -0600

    Add basic Raylib setup

--- arena/main.c
+++ arena/main.c
@@ -1,3 +1,19 @@
-int main(void) {
+#include "raylib.h"
 
+int main(void) {
+  const int windowWidth = 800;
+  const int windowHeight = 450;
+
+  InitWindow(windowWidth, windowHeight, "Hello Raylib");
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("Hello Raylib!", 190, 200, 20, LIGHTGRAY);
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
 }
