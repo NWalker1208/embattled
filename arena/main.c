8acf943c6e790de1ad9bbf49d8de37459e8bee51
Nathan Walker <nathanwalker1208@gmail.com>
2025-03-12 19:57:02 -0600

    Add 2D camera setup that auto scales to window size

--- arena/main.c
+++ arena/main.c
@@ -1,13 +1,34 @@
 #include "raylib.h"
+#include "math.h"
+
+#define WINDOW_MARGIN 20
+
+#define ARENA_WIDTH 500.0
+#define ARENA_HEIGHT 500.0
 
 int main(void) {
-  const int windowWidth = 800;
-  const int windowHeight = 450;
+  int windowWidth = 800;
+  int windowHeight = 450;
 
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(windowWidth, windowHeight, "Hello Raylib");
+  SetWindowMinSize(WINDOW_MARGIN, WINDOW_MARGIN);
+
+  Camera2D camera = { 0 };
+  camera.target = (Vector2){ 0, 0 };
+  camera.offset = (Vector2){ ARENA_WIDTH / 2, ARENA_HEIGHT / 2 };
+  camera.rotation = 0.0f;
+  camera.zoom = 1.0f;
+
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
+    windowWidth = GetScreenWidth(); windowHeight = GetScreenHeight();
+
+    // Update camera based on current window size
+    camera.offset = (Vector2){ windowWidth / 2, windowHeight / 2 };
+    camera.zoom = fmin((windowWidth - WINDOW_MARGIN) / ARENA_WIDTH, (windowHeight - WINDOW_MARGIN) / ARENA_HEIGHT);
+
     BeginDrawing();
     ClearBackground(RAYWHITE);
     DrawText("Hello Raylib!", 190, 200, 20, LIGHTGRAY);
