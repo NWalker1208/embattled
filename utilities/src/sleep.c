2fd61627909e6a293f73b5687b839136a34f68a1
Nathan Walker <nathanwalker1208@gmail.com>
2025-03-22 12:07:09 -0600

    Change the define being checked for in sleep.c

--- utilities/src/sleep.c
+++ utilities/src/sleep.c
@@ -1,13 +1,13 @@
 #include "utilities/sleep.h"
 
-#if defined(_MSC_VER)
+#if defined(WIN32)
 #include <windows.h>
 #else
 #include <unistd.h>
 #endif
 
 void psleep(unsigned int durationMs) {
-  #if defined(_MSC_VER)
+  #if defined(WIN32)
   Sleep(durationMs);
   #else
   usleep(durationMs * 1000);
