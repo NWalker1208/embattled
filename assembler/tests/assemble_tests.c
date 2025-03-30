6161d0dbfe6f5733a81112819f11985055dff0aa
Nathan Walker <nathanwalker1208@gmail.com>
2025-03-29 20:07:39 -0600

    Update remaining assembler unit tests

--- assembler/tests/assemble_tests.c
+++ assembler/tests/assemble_tests.c
@@ -1,6 +1,7 @@
 #define UNITY_SUPPORT_TEST_CASES
 #include <unity.h>
 #include <string.h>
+#include "test_utilities/text_assertions.h"
 #include "assembler/assemble.h"
 #include "parser/parse.h"
 #include "processor/process.h"
@@ -48,51 +49,28 @@ void test_TryAssembleProgram_should_outputInstructionBytes_when_lineIsValidAssem
   TEST_ASSERT_EQUAL_MEMORY(expectedMemory, memory, sizeof(memory));
 }
 
-void test_tryAssemble_should_outputDataBytes_when_lineIsValidAssemblyData(void) {
+void test_TryAssembleProgram_should_outputDataBytes_when_lineIsValidAssemblyData(void) {
   // Arrange
-  unsigned char bytes[] = { 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0 };
-  struct AssemblyLine lines[] = {
-    {
-      .kind = DATA,
-      .data = {
-        .length = sizeof(bytes),
-        .bytes = bytes,
-      },
-    }
-  };
+  initializeProgram(".data 12 34 56 78 9A BC DE F0");
 
+  unsigned char bytes[] = { 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0 };
   memcpy(expectedMemory, bytes, sizeof(bytes));
 
   // Act
-  bool success = tryAssemble(lines, 1, memory, &error);
+  bool success = TryAssembleProgram(&program, memory, &error);
 
   // Assert
   TEST_ASSERT_TRUE_MESSAGE(success, error.message);
   TEST_ASSERT_EQUAL_MEMORY(expectedMemory, memory, sizeof(memory));
 }
 
-void test_tryAssemble_should_outputInstructionWithReference_when_instructionReferencesInstruction(void) {
+void test_TryAssembleProgram_should_outputInstructionWithReference_when_instructionReferencesInstruction(void) {
   // Arrange
-  struct AssemblyParameter parameters[] = {
-    { .kind = LABEL_REFERENCE, .referencedLabel = "test" },
-  };
-  struct AssemblyLine lines[] = {
-    {
-      .kind = INSTRUCTION,
-      .instruction = {
-        .opcode = JMP,
-        .parameterCount = 1,
-        .parameters = parameters,
-      },
-    },
-    {
-      .label = "test",
-      .kind = INSTRUCTION,
-      .instruction = {
-        .opcode = NOP
-      },
-    },
-  };
+  initializeProgram(
+    "  jmp @test\n"
+    "test:\n"
+    "  nop"
+  );
 
   storeInstruction(expectedMemory, 0x0000, (struct Instruction){ 
     .opcode = JMP,
@@ -103,256 +81,161 @@ void test_tryAssemble_should_outputInstructionWithReference_when_instructionRefe
   });
 
   // Act
-  bool success = tryAssemble(lines, 2, memory, &error);
+  bool success = TryAssembleProgram(&program, memory, &error);
 
   // Assert
   TEST_ASSERT_TRUE_MESSAGE(success, error.message);
   TEST_ASSERT_EQUAL_MEMORY(expectedMemory, memory, sizeof(memory));
 }
 
-void test_tryAssemble_should_outputInstructionWithReference_when_instructionReferencesData(void) {
+void test_TryAssembleProgram_should_outputInstructionWithReference_when_instructionReferencesData(void) {
   // Arrange
-  struct AssemblyParameter parameters[] = {
-    { .kind = LABEL_REFERENCE, .referencedLabel = "test" },
-  };
-  unsigned char bytes[] = { 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0 };
-  struct AssemblyLine lines[] = {
-    {
-      .kind = INSTRUCTION,
-      .instruction = {
-        .opcode = LDIW,
-        .parameterCount = 1,
-        .parameters = parameters,
-      },
-    },
-    {
-      .label = "test",
-      .kind = DATA,
-      .data = {
-        .length = sizeof(bytes),
-        .bytes = bytes,
-      }
-    },
-  };
+  initializeProgram(
+    "  ldiw @test\n"
+    "test:\n"
+    "  .data 12 34 56 78 9A BC DE F0"
+  );
 
   storeInstruction(expectedMemory, 0x0000, (struct Instruction){ 
     .opcode = LDIW,
     .parameters.immediate.u16 = 0x0003,
   });
+  unsigned char bytes[] = { 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0 };
   memcpy(&expectedMemory[3], bytes, sizeof(bytes));
 
   // Act
-  bool success = tryAssemble(lines, 2, memory, &error);
+  bool success = TryAssembleProgram(&program, memory, &error);
 
   // Assert
   TEST_ASSERT_TRUE_MESSAGE(success, error.message);
   TEST_ASSERT_EQUAL_MEMORY(expectedMemory, memory, sizeof(memory));
 }
 
-void test_tryAssemble_should_outputErrorTooFewParams_when_instructionHasTooFewParams(void) {
+void test_TryAssembleProgram_should_outputErrorTooFewParams_when_instructionHasTooFewParams(void) {
   // Arrange
-  struct AssemblyParameter parameters[] = {
-    { .kind = REGISTER, .registerName = X0 },
-  };
-  struct AssemblyLine lines[] = {
-    {
-      .kind = INSTRUCTION,
-      .instruction = {
-        .opcode = ADD,
-        .parameterCount = 1,
-        .parameters = parameters,
-      },
-    }
-  };
+  initializeProgram("add $x0");
+
+  TextSpan expectedErrorSpan = {{0, 0}, {0, 7}};
 
   // Act
-  bool success = tryAssemble(lines, 1, memory, &error);
+  bool success = TryAssembleProgram(&program, memory, &error);
 
   // Assert
   TEST_ASSERT_FALSE(success);
   TEST_ASSERT_EQUAL_STRING(TOO_FEW_PARAMS, error.message);
-  TEST_ASSERT_EQUAL_INT(0, error.lineNumber);
+  TEST_ASSERT_EQUIVALENT_TEXT_SPAN(expectedErrorSpan, error.sourceSpan, program.sourceText);
 }
 
-void test_tryAssemble_should_outputErrorTooManyParams_when_instructionHasTooManyParams(void) {
+void test_TryAssembleProgram_should_outputErrorTooManyParams_when_instructionHasTooManyParams(void) {
   // Arrange
-  struct AssemblyParameter parameters[] = {
-    { .kind = REGISTER, .registerName = X0 },
-    { .kind = REGISTER, .registerName = X1 },
-    { .kind = REGISTER, .registerName = X2 },
-  };
-  struct AssemblyLine lines[] = {
-    {
-      .kind = INSTRUCTION,
-      .instruction = {
-        .opcode = ADD,
-        .parameterCount = 3,
-        .parameters = parameters,
-      },
-    }
-  };
+  initializeProgram("add $x0, $x1, $x2");
+
+  TextSpan expectedErrorSpan = {{0, 0}, {0, 17}};
 
   // Act
-  bool success = tryAssemble(lines, 1, memory, &error);
+  bool success = TryAssembleProgram(&program, memory, &error);
 
   // Assert
   TEST_ASSERT_FALSE(success);
   TEST_ASSERT_EQUAL_STRING(TOO_MANY_PARAMS, error.message);
-  TEST_ASSERT_EQUAL_INT(0, error.lineNumber);
+  TEST_ASSERT_EQUIVALENT_TEXT_SPAN(expectedErrorSpan, error.sourceSpan, program.sourceText);
 }
 
-void test_tryAssemble_should_outputErrorExpectedRegister_when_instructionHasImmediateValueInsteadOfRegister(void) {
+void test_TryAssembleProgram_should_outputErrorExpectedRegister_when_instructionHasImmediateValueInsteadOfRegister(void) {
   // Arrange
-  struct AssemblyParameter parameters[] = {
-    { .kind = REGISTER, .registerName = X0 },
-    { .kind = IMMEDIATE_VALUE, .immediateValue = 0x1234 },
-  };
-  struct AssemblyLine lines[] = {
-    {
-      .kind = INSTRUCTION,
-      .instruction = {
-        .opcode = ADD,
-        .parameterCount = 2,
-        .parameters = parameters,
-      },
-    }
-  };
+  initializeProgram("add $x0, 0x1234");
+  
+  TextSpan expectedErrorSpan = {{0, 9}, {0, 15}};
 
   // Act
-  bool success = tryAssemble(lines, 1, memory, &error);
+  bool success = TryAssembleProgram(&program, memory, &error);
 
   // Assert
   TEST_ASSERT_FALSE(success);
   TEST_ASSERT_EQUAL_STRING(EXPECTED_REGISTER, error.message);
-  TEST_ASSERT_EQUAL_INT(0, error.lineNumber);
+  TEST_ASSERT_EQUIVALENT_TEXT_SPAN(expectedErrorSpan, error.sourceSpan, program.sourceText);
 }
 
-void test_tryAssemble_should_outputErrorExpectedImmediateValue_when_instructionHasRegisterInsteadOfUnder16BitImmediateValue(void) {
+void test_TryAssembleProgram_should_outputErrorExpectedImmediateValue_when_instructionHasRegisterInsteadOfUnder16BitImmediateValue(void) {
   // Arrange
-  struct AssemblyParameter parameters[] = {
-    { .kind = REGISTER, .registerName = X0 },
-    { .kind = REGISTER, .registerName = X1 },
-  };
-  struct AssemblyLine lines[] = {
-    {
-      .kind = INSTRUCTION,
-      .instruction = {
-        .opcode = INC,
-        .parameterCount = 2,
-        .parameters = parameters,
-      },
-    }
-  };
+  initializeProgram("inc $x0, $x1");
+  
+  TextSpan expectedErrorSpan = {{0, 9}, {0, 12}};
 
   // Act
-  bool success = tryAssemble(lines, 1, memory, &error);
+  bool success = TryAssembleProgram(&program, memory, &error);
 
   // Assert
   TEST_ASSERT_FALSE(success);
   TEST_ASSERT_EQUAL_STRING(EXPECTED_IMMEDIATE_VALUE, error.message);
-  TEST_ASSERT_EQUAL_INT(0, error.lineNumber);
+  TEST_ASSERT_EQUIVALENT_TEXT_SPAN(expectedErrorSpan, error.sourceSpan, program.sourceText);
 }
 
-void test_tryAssemble_should_outputErrorExpectedImmediateValueOrLabelRef_when_instructionHasRegisterInsteadOf16BitImmediateValue(void) {
+void test_TryAssembleProgram_should_outputErrorExpectedImmediateValueOrLabelRef_when_instructionHasRegisterInsteadOf16BitImmediateValue(void) {
   // Arrange
-  struct AssemblyParameter parameters[] = {
-    { .kind = REGISTER, .registerName = X0 },
-  };
-  struct AssemblyLine lines[] = {
-    {
-      .kind = INSTRUCTION,
-      .instruction = {
-        .opcode = JMP,
-        .parameterCount = 1,
-        .parameters = parameters,
-      },
-    }
-  };
+  initializeProgram("jmp $x0");
+
+  TextSpan expectedErrorSpan = {{0, 4}, {0, 7}};
 
   // Act
-  bool success = tryAssemble(lines, 1, memory, &error);
+  bool success = TryAssembleProgram(&program, memory, &error);
 
   // Assert
   TEST_ASSERT_FALSE(success);
   TEST_ASSERT_EQUAL_STRING(EXPECTED_IMMEDIATE_VALUE_OR_LABEL_REF, error.message);
-  TEST_ASSERT_EQUAL_INT(0, error.lineNumber);
+  TEST_ASSERT_EQUIVALENT_TEXT_SPAN(expectedErrorSpan, error.sourceSpan, program.sourceText);
 }
 
-TEST_CASE(JMP, false, -1)
-TEST_CASE(JMP, false, 0x10000)
-TEST_CASE(LDIB, false, -1)
-TEST_CASE(LDIB, false, 0x100)
-TEST_CASE(INC, true, -1)
-TEST_CASE(INC, true, 0x10)
-TEST_CASE(LDMB, true, -9)
-TEST_CASE(LDMB, true, 8)
-void test_tryAssemble_should_outputErrorImmediateValueOutOfRange_when_instructionHasImmediateValueOutOfRange(enum Opcode opcode, bool needsRegister, signed int immediateValue) {
+TEST_CASE("jmp -1", 4, 6)
+TEST_CASE("jmp 0x10000", 4, 11)
+TEST_CASE("ldib -1", 5, 7)
+TEST_CASE("ldib 0x100", 5, 10)
+TEST_CASE("inc $x0, -1", 9, 11)
+TEST_CASE("inc $x0, 0x10", 9, 13)
+TEST_CASE("ldmb $x0, -9", 10, 12)
+TEST_CASE("ldmb $x0, 8", 10, 11)
+void test_TryAssembleProgram_should_outputErrorImmediateValueOutOfRange_when_instructionHasImmediateValueOutOfRange(const char* source, size_t errorSpanStartCol, size_t errorSpanEndCol) {
   // Arrange
-  struct AssemblyParameter parameters[] = {
-    { .kind = REGISTER, .registerName = X0 },
-    { .kind = IMMEDIATE_VALUE, .immediateValue = immediateValue },
-  };
-  struct AssemblyLine lines[] = {
-    {
-      .kind = INSTRUCTION,
-      .instruction = {
-        .opcode = opcode,
-        .parameterCount = needsRegister ? 2 : 1,
-        .parameters = needsRegister ? parameters : parameters + 1,
-      },
-    }
-  };
+  initializeProgram(source);
+
+  TextSpan expectedErrorSpan = {{0, errorSpanStartCol}, {0, errorSpanEndCol}};
 
   // Act
-  bool success = tryAssemble(lines, 1, memory, &error);
+  bool success = TryAssembleProgram(&program, memory, &error);
 
   // Assert
   TEST_ASSERT_FALSE(success);
   TEST_ASSERT_EQUAL_STRING(IMMEDIATE_VALUE_OUT_OF_RANGE, error.message);
-  TEST_ASSERT_EQUAL_INT(0, error.lineNumber);
+  TEST_ASSERT_EQUIVALENT_TEXT_SPAN(expectedErrorSpan, error.sourceSpan, program.sourceText);
 }
 
-void test_tryAssemble_should_outputErrorInvalidInstruction_when_instructionHasUndefinedOpcode(void) {
+void test_TryAssembleProgram_should_outputErrorInvalidInstruction_when_instructionHasUndefinedOpcode(void) {
   // Arrange
-  struct AssemblyLine lines[] = {
-    {
-      .kind = INSTRUCTION,
-      .instruction = {
-        .opcode = OPCODE_COUNT,
-      },
-    }
-  };
+  initializeProgram("nop");
+  program.lines[0].instruction.opcode = OPCODE_COUNT; // Not a valid opcode
+
+  TextSpan expectedErrorSpan = {{0, 0}, {0, 3}};
 
   // Act
-  bool success = tryAssemble(lines, 1, memory, &error);
+  bool success = TryAssembleProgram(&program, memory, &error);
 
   // Assert
   TEST_ASSERT_FALSE(success);
   TEST_ASSERT_EQUAL_STRING(INVALID_INSTRUCTION, error.message);
-  TEST_ASSERT_EQUAL_INT(0, error.lineNumber);
+  TEST_ASSERT_EQUIVALENT_TEXT_SPAN(expectedErrorSpan, error.sourceSpan, program.sourceText);
 }
 
-void test_tryAssemble_should_outputErrorUndefinedLabel_when_instructionReferencesUndefinedLabel(void) {
+void test_TryAssembleProgram_should_outputErrorUndefinedLabel_when_instructionReferencesUndefinedLabel(void) {
   // Arrange
-  struct AssemblyParameter parameters[] = {
-    { .kind = LABEL_REFERENCE, .referencedLabel = "test" },
-  };
-  struct AssemblyLine lines[] = {
-    {
-      .kind = INSTRUCTION,
-      .instruction = {
-        .opcode = JMP,
-        .parameterCount = 1,
-        .parameters = parameters,
-      },
-    }
-  };
+  initializeProgram("jmp @test");
+
+  TextSpan expectedErrorSpan = {{0, 4}, {0, 9}};
 
   // Act
-  bool success = tryAssemble(lines, 1, memory, &error);
+  bool success = TryAssembleProgram(&program, memory, &error);
 
   // Assert
   TEST_ASSERT_FALSE(success);
-  TEST_ASSERT_EQUAL_STRING(UNDEFINED_LABEL, error.message);
-  TEST_ASSERT_EQUAL_INT(0, error.lineNumber);
+  TEST_ASSERT_EQUAL_STRING(UNDEFINED_LABEL_NAME, error.message);
+  TEST_ASSERT_EQUIVALENT_TEXT_SPAN(expectedErrorSpan, error.sourceSpan, program.sourceText);
 }
