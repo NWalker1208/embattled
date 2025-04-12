610a4449f08e93c6ddeb1226c6651f17c5cdf046
Nathan Walker <nathanwalker1208@gmail.com>
2025-04-12 10:35:49 -0600

    Refactor simulation.h to split robot-related code into separate file

---
+++ arena/include/arena/robot.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <raylib.h>
+#include "processor/process.h"
+
+#define ROBOT_INITIAL_ENERGY 4000000
+#define ROBOT_WEAPON_COOLDOWN_STEPS 1000
+#define ROBOT_NUM_SENSORS 3
+
+
+typedef struct {
+  // The index of the physics body representing this robot.
+  const unsigned int physicsBodyIndex;
+  
+  // The amount of energy that the robot has left. Also represents the robot's health.
+  int energyRemaining;
+
+  // The number of simulation steps until the robot's weapon is ready to use.
+  int weaponCooldownRemaining;
+  // The start and end points of the robot's last weapon fire.
+  struct {
+    Vector2 start, end;
+  } lastWeaponFire;
+
+  // The current start and end points of the robot's sensors.
+  struct {
+    Vector2 start, end;
+  } sensors[ROBOT_NUM_SENSORS];
+
+  // The state of the robot's processor.
+  struct ProcessState processState;
+} Robot;
+
+Robot InitRobot(size_t physicsBodyIndex);
--- arena/include/arena/simulation.h
+++ arena/include/arena/simulation.h
@@ -3,58 +3,40 @@
 #include <stdbool.h>
 #include "arena/physics.h"
 #include "processor/process.h"
+#include "arena/robot.h"
 
-#define MAX_ROBOTS 2
+#define SIMULATION_MAX_ROBOTS 2
+#define SIMULATION_NEUTRAL_TIME_SCALE 1024
 
-#define ROBOT_INITIAL_ENERGY 4000000
-#define ROBOT_WEAPON_COOLDOWN_STEPS 1000
-#define ROBOT_NUM_SENSORS 9
 
-#define NEUTRAL_TIME_SCALE 1024
-
-// A robot that is being simulated.
+// The state of a simulation.
 typedef struct {
-  // The index of the physics body representing this robot.
-  const unsigned int physicsBodyIndex;
-  
-  // The amount of energy that the robot has left. Also represents the robot's health.
-  int energyRemaining;
-  // The number of simulation steps until the robot's weapon is ready to use.
-  int weaponCooldownRemaining;
-  // The start and end points of the robot's last weapon fire.
-  struct {
-    Vector2 start, end;
-  } lastWeaponFire;
-
-  // The start and end points of the robot's sensors.
-  struct {
-    Vector2 start, end;
-  } sensors[ROBOT_NUM_SENSORS];
-
-  // The state of the robot's processor.
-  struct ProcessState processState;
-} Robot;
-
-// The arguments expected by the StartSimulation function.
-typedef struct {
-  // The number of robots being simulated.
-  unsigned int robotCount;
-  // The array of robots being simulated.
-  Robot robots[MAX_ROBOTS];
-  // The physics world containing the robots being simulated.
-  PhysicsWorld physicsWorld;
   // A mutex protecting reads/writes to the robots and physics world.
   pthread_mutex_t mutex;
+  
+  // The physics world being simulated.
+  PhysicsWorld physicsWorld;
+
+  // The number of robots being simulated.
+  size_t robotCount;
+  // The array of robots being simulated.
+  Robot robots[SIMULATION_MAX_ROBOTS];
+
   // The scaling factor applied to the passage of time in the simulation.
   //   0 = Paused
-  //   NEUTRAL_TIME_SCALE = Same as realtime
+  //   NEUTRAL_TIME_SCALE = Realtime
   //   UINT_MAX = Unlimited
   unsigned int timeScale;
   // Whether a simulation step should occur on the next iteration regardless of the current time scale.
   bool forceStep;
+
   // Whether the simulation loop should stop.
   bool shouldStop;
-} SimulationArguments;
+} Simulation;
 
-// Starts the simulation loop. Expects a pointer to SimulationArguments.
+// Initializes a simulation with the given number of robots.
+Simulation InitSimulation(size_t robotCount);
+
+// Starts the simulation loop on the current thread. Expects a pointer to a Simulation.
+// Returns NULL.
 void* StartSimulation(void* arg);
