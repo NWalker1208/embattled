4f2966db4c1b2374485a714205e9e04a4bf43f7e
Nathan Walker <nathanwalker1208@gmail.com>
2025-04-26 13:46:44 -0600

    Add support for emscripten sleep

--- utilities/src/sleep.c
+++ utilities/src/sleep.c
@@ -2,6 +2,8 @@
 
 #if defined(WIN32)
 #include <windows.h>
+#elif defined(__EMSCRIPTEN__)
+#include <emscripten.h>
 #else
 #include <unistd.h>
 #endif
@@ -9,6 +11,8 @@
 void psleep(unsigned int durationMs) {
   #if defined(WIN32)
   Sleep(durationMs);
+  #elif defined(__EMSCRIPTEN__)
+  emscripten_sleep(durationMs);
   #else
   usleep(durationMs * 1000);
   #endif
