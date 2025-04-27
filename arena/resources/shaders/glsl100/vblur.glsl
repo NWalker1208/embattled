134f686537c6788a15561fc333ff8d27cb75561b
Nathan Walker <nathanwalker1208@gmail.com>
2025-04-27 11:10:16 -0600

    Fix shader compilation errors

--- arena/resources/shaders/glsl100/vblur.glsl
+++ arena/resources/shaders/glsl100/vblur.glsl
@@ -12,6 +12,8 @@ uniform int renderWidth;
 uniform int renderHeight;
 uniform float blurSize;
 
+const int MAX_BLUR_SIZE = 10;
+
 void main() {
   int intBlurSize = int(ceil(abs(blurSize)));
   float stdDev = blurSize * 0.4;
@@ -19,11 +21,13 @@ void main() {
   vec4 weightedAverage = vec4(0,0,0,0);
   float totalWeight = 0.0;
 
-  for (int offset = -intBlurSize; offset <= intBlurSize; offset++) {
-    float temp = offset / stdDev;
+  for (int i = 0; i <= 2 * MAX_BLUR_SIZE; i++) {
+    if (i > 2 * intBlurSize) { break; }
+    int offset = i - intBlurSize;
+    float temp = float(offset) / stdDev;
     float weight = exp(-(temp * temp));
 
-    weightedAverage += weight * texture2D(texture0, vec2(fragTexCoord.x, fragTexCoord.y + float(offset)/renderHeight)); 
+    weightedAverage += weight * texture2D(texture0, vec2(fragTexCoord.x, fragTexCoord.y + float(offset)/float(renderHeight))); 
     totalWeight += weight;
   }
 
