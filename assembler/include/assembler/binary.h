56776bf9f34b22cc079b4fbf73e31bb8c7f570dd
Nathan Walker <nathanwalker1208@gmail.com>
2025-05-10 18:41:47 -0600

    Update binary.h to use new conventions

--- assembler/include/assembler/binary.h
+++ assembler/include/assembler/binary.h
@@ -4,21 +4,21 @@
 #include "processor/process.h"
 
 // A mapping between an assembly line and a memory address.
-typedef struct {
+typedef struct AssemblyMemoryMapping {
   size_t assemblyLineIndex;
-  unsigned short memoryAddress;
+  uint16_t memoryAddress;
 } AssemblyMemoryMapping;
 
 // An assembled binary program.
 // TODO: Make this the output of TryAssembleProgram instead of a plain memory array.
-typedef struct {
+typedef struct BinaryProgram {
   // The number of assembly-memory mappings.
   size_t assemblyMemoryMappingsCount;
   // The array of assembly-memory mappings.
   AssemblyMemoryMapping* assemblyMemoryMappings;
 
   // The bytes of the program.
-  unsigned char bytes[MEMORY_SIZE];
+  uint8_t bytes[MEMORY_SIZE];
 } BinaryProgram;
 
 // Frees the memory referenced by the BinaryProgram struct.
