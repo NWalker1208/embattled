b41b913278e98e06deb55fb4ae22ff556170fe12
Nathan Walker <nathanwalker1208@gmail.com>
2025-05-10 18:35:16 -0600

    Refactor the data structures for assembly code

--- assembler/include/assembler/assembly.h
+++ assembler/include/assembler/assembly.h
@@ -1,69 +1,72 @@
 #pragma once
 #include <stdint.h>
+#include "assembler/mnemonic.h"
 #include "utilities/text.h"
 #include "processor/opcode.h"
 #include "processor/register.h"
 
+#define MAX_ASSEMBLY_OPERANDS 3
+
 // TODO: Add support for storing references in data sections
 // TODO: Add support for relative references?
 
-// The label represented by an assembly line.
+// A label represented by a line of assembly code.
 typedef struct {
   // The span of the name portion of the label. Empty if name is not present.
   TextSpan nameSpan;
   // The span of the address portion of the label. Empty if address is not present.
   TextSpan addressSpan;
   // The value of the address portion of the label, if present.
-  unsigned short address;
+  uint16_t address;
 } AssemblyLabel;
 
-// The kind of an assembly parameter.
-typedef enum {
-  ASSEMBLY_PARAM_REGISTER,
-  ASSEMBLY_PARAM_IMMEDIATE,
-  ASSEMBLY_PARAM_LABEL,
-} AssemblyParameterKind;
+// The kind of an assembly operand.
+typedef enum AssemblyOperandKind {
+  ASSEMBLY_OPERAND_REGISTER,
+  ASSEMBLY_OPERAND_IMMEDIATE,
+  ASSEMBLY_OPERAND_LABEL,
+} AssemblyOperandKind;
 
-// The parameter of an assembly instruction.
-typedef struct {
+// An operand of an assembly instruction.
+typedef struct AssemblyOperand {
   // The location of the parameter in the source TextContents.
   TextSpan sourceSpan;
   // The kind of parameter that this is.
-  AssemblyParameterKind kind;
+  AssemblyOperandKind kind;
   union {
-    enum Register registerName; // kind == ASSEMBLY_PARAM_REGISTER
-    signed int immediateValue;  // kind == ASSEMBLY_PARAM_IMMEDIATE
-    TextSpan labelSpan;         // kind == ASSEMBLY_PARAM_LABEL
+    Register registerName;  // kind == ASSEMBLY_OPERAND_REGISTER
+    int32_t immediateValue; // kind == ASSEMBLY_OPERAND_IMMEDIATE
+    TextSpan labelSpan;     // kind == ASSEMBLY_OPERAND_LABEL
   };
-} AssemblyParameter;
+} AssemblyOperand;
 
-// The instruction represented by an assembly line.
-typedef struct {
-  // The opcode for this instruction.
-  enum Opcode opcode;
-  // The number of parameters in the parameters array.
-  size_t parameterCount;
-  // The parameters given to this instruction. Should be a dynamically allocated array.
-  AssemblyParameter* parameters;
+// An instruction represented by a line of assembly code.
+typedef struct AssemblyInstruction {
+  // The mnemonic for this instruction's opcode.
+  AssemblyMnemonic mnemonic;
+  // The number of items in the operands array (up to MAX_ASSEMBLY_OPERANDS).
+  size_t operandCount;
+  // The operands of this instruction.
+  AssemblyOperand operands[MAX_ASSEMBLY_OPERANDS];
 } AssemblyInstruction;
 
-// The data represented by an assembly line.
-typedef struct {
-  // The number of bytes in the bytes array.
-  size_t length;
-  // The bytes of this data. Should be a dynamically allocated array.
-  unsigned char* bytes;
+// Some data represented by a line of assembly code.
+typedef struct AssemblyData {
+  // The index of the start of this data in the program data buffer.
+  size_t startIndex;
+  // The number of bytes in this data.
+  size_t byteCount;
 } AssemblyData;
 
-// The kind of an assembly line.
-typedef enum {
+// The kind of a line of assembly code.
+typedef enum AssemblyLineKind {
   ASSEMBLY_LINE_LABEL,
   ASSEMBLY_LINE_INSTRUCTION,
   ASSEMBLY_LINE_DATA,
 } AssemblyLineKind;
 
-// A line of an assembly program.
-typedef struct {
+// A line of assembly code.
+typedef struct AssemblyLine {
   // The location of the line in the source TextContents.
   TextSpan sourceSpan;
   // The kind of line that this is.
@@ -76,21 +79,14 @@ typedef struct {
 } AssemblyLine;
 
 // An assembly program consisting of zero or more lines.
-typedef struct {
+typedef struct AssemblyProgram {
   // The number of lines in the assembly program.
   size_t lineCount;
   // The lines of the assembly program. Should be a dynamically allocated array.
   AssemblyLine* lines;
+  // The buffer for any assembly data in the program. Should be a dynamically allocated array.
+  uint8_t* dataBuffer;
 } AssemblyProgram;
 
 // Frees all memory referenced by the assembly program and replaces freed pointers with NULL.
 void DestroyAssemblyProgram(AssemblyProgram* program);
-
-// Frees all memory referenced by the assembly line and replaces freed pointers with NULL.
-void DestroyAssemblyLine(AssemblyLine* line);
-
-// Frees all memory referenced by the assembly instruction and replaces freed pointers with NULL.
-void DestroyAssemblyInstruction(AssemblyInstruction* instruction);
-
-// Frees all memory referenced by the assembly data and replaces freed pointers with NULL.
-void DestroyAssemblyData(AssemblyData* data);
--- assembler/src/assembly.c
+++ assembler/src/assembly.c
@@ -5,30 +5,6 @@ void DestroyAssemblyProgram(AssemblyProgram* program) {
   free(program->lines);
   program->lines = NULL;
   program->lineCount = 0;
-}
-
-void DestroyAssemblyLine(AssemblyLine* line) {
-  switch (line->kind) {
-    case ASSEMBLY_LINE_INSTRUCTION: {
-      DestroyAssemblyInstruction(&line->instruction);
-      break;
-    }
-    case ASSEMBLY_LINE_DATA: {
-      DestroyAssemblyData(&line->data);
-      break;
-    }
-    default: break;
-  }
-}
-
-void DestroyAssemblyInstruction(AssemblyInstruction* instruction) {
-  free(instruction->parameters);
-  instruction->parameters = NULL;
-  instruction->parameterCount = 0;
-}
-
-void DestroyAssemblyData(AssemblyData* data) {
-  free(data->bytes);
-  data->bytes = NULL;
-  data->length = 0;
+  free(program->dataBuffer);
+  program->dataBuffer = NULL;
 }
