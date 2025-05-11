435282f4b84380d6aca5520d5169bd5bacbc6035
Nathan Walker <nathanwalker1208@gmail.com>
2025-05-11 11:44:31 -0600

    Create data structures for representing assembly mnemonic info

--- assembler/include/assembler/mnemonic.h
+++ assembler/include/assembler/mnemonic.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <stdint.h>
+#include <stdbool.h>
+#include "assembler/operand.h"
+
+#define MAX_MNEMONIC_OVERLOADS 4
 
 // An identifier that maps to one or more opcodes depending on the parameters provided.
 typedef enum AssemblyMnemonic {
@@ -41,3 +46,22 @@ typedef enum AssemblyMnemonic {
 
   ASSEMBLY_MNEMONIC_COUNT
 } AssemblyMnemonic;
+
+// A particular overload of an assembly mnemonic. Matches a specific sequence of operand kinds and assembles to a specific opcode.
+typedef struct AssemblyMnemonicOverload {
+  size_t operandCount; // The number of operands this overload accepts.
+  AssemblyOperandKind operandKinds[MAX_ASSEMBLY_OPERANDS]; // The kind of each operand accepted by this overload.
+  bool swapRegisterBAndRegisterC; // Whether register C appears before register B (used by some pseudo-instructions).
+  Opcode opcode; // The opcode that this overload assembles into.
+} AssemblyMnemonicOverload;
+
+// Describes the details of a particular assembly mnemonic.
+typedef struct AssemblyMnemonicInfo {
+  const char* identifier; // The identifier of the mnemonic as a string.
+  size_t overloadCount; // The number of overloads of the mnemonic.
+  AssemblyMnemonicOverload overloads[MAX_MNEMONIC_OVERLOADS]; // The overloads of the mnemonic.
+} AssemblyMnemonicInfo;
+
+// Gets the info for the specified assembly mnemonic.
+// If the mnemonic is invalid, returns NULL.
+const AssemblyMnemonicInfo* getAssemblyMnemonicInfo(AssemblyMnemonic mnemonic);
