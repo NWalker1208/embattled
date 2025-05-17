906346be59b4bcb3590d89d1ff741ace0acfa74e
Nathan Walker <nathanwalker1208@gmail.com>
2025-05-17 16:10:18 -0600

    Update unit tests for TryAssembleProgram

--- assembler/tests/assemble_tests.c
+++ assembler/tests/assemble_tests.c
@@ -36,12 +36,13 @@ void initializeProgram(const char* source) {
 
 void test_TryAssembleProgram_should_outputInstructionBytes_when_lineIsValidAssemblyInstruction(void) {
   // Arrange
-  initializeProgram("add $x0, $x1");
+  initializeProgram("add $x0, $x1, $x2");
 
-  storeInstruction(expectedMemory, 0x0000, (struct Instruction){ 
-    .opcode = ADD,
-    .parameters.registerA = X0,
-    .parameters.registerB = X1
+  writeInstruction(expectedMemory, 0x0000, (struct Instruction){ 
+    .opcode = OPCODE_ADD_R,
+    .operands.registerA = REGISTER_X0,
+    .operands.registerB = REGISTER_X1,
+    .operands.registerC = REGISTER_X2
   });
 
   // Act
@@ -75,12 +76,12 @@ void test_TryAssembleProgram_should_outputInstructionWithReference_when_instruct
     "  nop"
   );
 
-  storeInstruction(expectedMemory, 0x0000, (struct Instruction){ 
-    .opcode = JMP,
-    .parameters.immediate.u16 = 0x0003,
+  writeInstruction(expectedMemory, 0x0000, (struct Instruction){ 
+    .opcode = OPCODE_JMP_I,
+    .operands.immediateA.u16 = 0x0003,
   });
-  storeInstruction(expectedMemory, 0x0003, (struct Instruction){
-    .opcode = NOP,
+  writeInstruction(expectedMemory, 0x0003, (struct Instruction){
+    .opcode = OPCODE_NOP,
   });
 
   // Act
@@ -94,17 +95,18 @@ void test_TryAssembleProgram_should_outputInstructionWithReference_when_instruct
 void test_TryAssembleProgram_should_outputInstructionWithReference_when_instructionReferencesData(void) {
   // Arrange
   initializeProgram(
-    "  ldiw @test\n"
+    "  set $x0, @test\n"
     "test:\n"
     "  .data 12 34 56 78 9A BC DE F0"
   );
 
-  storeInstruction(expectedMemory, 0x0000, (struct Instruction){ 
-    .opcode = LDIW,
-    .parameters.immediate.u16 = 0x0003,
+  writeInstruction(expectedMemory, 0x0000, (struct Instruction){ 
+    .opcode = OPCODE_SET_I,
+    .operands.registerA = REGISTER_X0,
+    .operands.immediateA.u16 = 0x0004,
   });
   unsigned char bytes[] = { 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0 };
-  memcpy(&expectedMemory[3], bytes, sizeof(bytes));
+  memcpy(&expectedMemory[4], bytes, sizeof(bytes));
 
   // Act
   bool success = TryAssembleProgram(&sourceText, &program, memory, &error);
@@ -114,108 +116,63 @@ void test_TryAssembleProgram_should_outputInstructionWithReference_when_instruct
   TEST_ASSERT_EQUAL_MEMORY(expectedMemory, memory, sizeof(memory));
 }
 
-void test_TryAssembleProgram_should_outputErrorTooFewParams_when_instructionHasTooFewParams(void) {
+void test_TryAssembleProgram_should_outputErrorNoMatchingOverload_when_instructionHasIncorrectNumberOfParams(void) {
   // Arrange
-  initializeProgram("add $x0");
+  initializeProgram("add $x0, $x1");
 
-  TextSpan expectedErrorSpan = {{0, 0}, {0, 7}};
+  TextSpan expectedErrorSpan = {{0, 0}, {0, 12}};
 
   // Act
   bool success = TryAssembleProgram(&sourceText, &program, memory, &error);
 
   // Assert
   TEST_ASSERT_FALSE(success);
-  TEST_ASSERT_EQUAL_STRING(TOO_FEW_PARAMS, error.message);
+  TEST_ASSERT_EQUAL_STRING(NO_MATCHING_OVERLOAD, error.message);
   TEST_ASSERT_EQUIVALENT_TEXT_SPAN(expectedErrorSpan, error.sourceSpan, sourceText);
 }
 
-void test_TryAssembleProgram_should_outputErrorTooManyParams_when_instructionHasTooManyParams(void) {
+void test_TryAssembleProgram_should_outputErrorNoMatchingOverload_when_instructionHasImmediateValueInsteadOfRegister(void) {
   // Arrange
-  initializeProgram("add $x0, $x1, $x2");
-
-  TextSpan expectedErrorSpan = {{0, 0}, {0, 17}};
-
-  // Act
-  bool success = TryAssembleProgram(&sourceText, &program, memory, &error);
-
-  // Assert
-  TEST_ASSERT_FALSE(success);
-  TEST_ASSERT_EQUAL_STRING(TOO_MANY_PARAMS, error.message);
-  TEST_ASSERT_EQUIVALENT_TEXT_SPAN(expectedErrorSpan, error.sourceSpan, sourceText);
-}
-
-void test_TryAssembleProgram_should_outputErrorExpectedRegister_when_instructionHasImmediateValueInsteadOfRegister(void) {
-  // Arrange
-  initializeProgram("add $x0, 0x1234");
+  initializeProgram("add 0x1234, $x0, $x1");
   
-  TextSpan expectedErrorSpan = {{0, 9}, {0, 15}};
+  TextSpan expectedErrorSpan = {{0, 0}, {0, 20}};
 
   // Act
   bool success = TryAssembleProgram(&sourceText, &program, memory, &error);
 
   // Assert
   TEST_ASSERT_FALSE(success);
-  TEST_ASSERT_EQUAL_STRING(EXPECTED_REGISTER, error.message);
+  TEST_ASSERT_EQUAL_STRING(NO_MATCHING_OVERLOAD, error.message);
   TEST_ASSERT_EQUIVALENT_TEXT_SPAN(expectedErrorSpan, error.sourceSpan, sourceText);
 }
 
-void test_TryAssembleProgram_should_outputErrorExpectedImmediateValue_when_instructionHasRegisterInsteadOfUnder16BitImmediateValue(void) {
-  // Arrange
-  initializeProgram("inc $x0, $x1");
-  
-  TextSpan expectedErrorSpan = {{0, 9}, {0, 12}};
+// TEST_CASE("jmp -1", 4, 6)
+// TEST_CASE("jmp 0x10000", 4, 11)
+// TEST_CASE("ldib -1", 5, 7)
+// TEST_CASE("ldib 0x100", 5, 10)
+// TEST_CASE("inc $x0, -1", 9, 11)
+// TEST_CASE("inc $x0, 0x10", 9, 13)
+// TEST_CASE("ldmb $x0, -9", 10, 12)
+// TEST_CASE("ldmb $x0, 8", 10, 11)
+// void test_TryAssembleProgram_should_outputErrorImmediateValueOutOfRange_when_instructionHasImmediateValueOutOfRange(const char* source, size_t errorSpanStartCol, size_t errorSpanEndCol) {
+//   // Arrange
+//   initializeProgram(source);
 
-  // Act
-  bool success = TryAssembleProgram(&sourceText, &program, memory, &error);
+//   TextSpan expectedErrorSpan = {{0, errorSpanStartCol}, {0, errorSpanEndCol}};
 
-  // Assert
-  TEST_ASSERT_FALSE(success);
-  TEST_ASSERT_EQUAL_STRING(EXPECTED_IMMEDIATE_VALUE, error.message);
-  TEST_ASSERT_EQUIVALENT_TEXT_SPAN(expectedErrorSpan, error.sourceSpan, sourceText);
-}
+//   // Act
+//   bool success = TryAssembleProgram(&sourceText, &program, memory, &error);
 
-void test_TryAssembleProgram_should_outputErrorExpectedImmediateValueOrLabelRef_when_instructionHasRegisterInsteadOf16BitImmediateValue(void) {
-  // Arrange
-  initializeProgram("jmp $x0");
+//   // Assert
+//   TEST_ASSERT_FALSE(success);
+//   TEST_ASSERT_EQUAL_STRING(IMMEDIATE_VALUE_OUT_OF_RANGE, error.message);
+//   TEST_ASSERT_EQUIVALENT_TEXT_SPAN(expectedErrorSpan, error.sourceSpan, sourceText);
+// }
 
-  TextSpan expectedErrorSpan = {{0, 4}, {0, 7}};
-
-  // Act
-  bool success = TryAssembleProgram(&sourceText, &program, memory, &error);
-
-  // Assert
-  TEST_ASSERT_FALSE(success);
-  TEST_ASSERT_EQUAL_STRING(EXPECTED_IMMEDIATE_VALUE_OR_LABEL_REF, error.message);
-  TEST_ASSERT_EQUIVALENT_TEXT_SPAN(expectedErrorSpan, error.sourceSpan, sourceText);
-}
-
-TEST_CASE("jmp -1", 4, 6)
-TEST_CASE("jmp 0x10000", 4, 11)
-TEST_CASE("ldib -1", 5, 7)
-TEST_CASE("ldib 0x100", 5, 10)
-TEST_CASE("inc $x0, -1", 9, 11)
-TEST_CASE("inc $x0, 0x10", 9, 13)
-TEST_CASE("ldmb $x0, -9", 10, 12)
-TEST_CASE("ldmb $x0, 8", 10, 11)
-void test_TryAssembleProgram_should_outputErrorImmediateValueOutOfRange_when_instructionHasImmediateValueOutOfRange(const char* source, size_t errorSpanStartCol, size_t errorSpanEndCol) {
-  // Arrange
-  initializeProgram(source);
-
-  TextSpan expectedErrorSpan = {{0, errorSpanStartCol}, {0, errorSpanEndCol}};
-
-  // Act
-  bool success = TryAssembleProgram(&sourceText, &program, memory, &error);
-
-  // Assert
-  TEST_ASSERT_FALSE(success);
-  TEST_ASSERT_EQUAL_STRING(IMMEDIATE_VALUE_OUT_OF_RANGE, error.message);
-  TEST_ASSERT_EQUIVALENT_TEXT_SPAN(expectedErrorSpan, error.sourceSpan, sourceText);
-}
-
-void test_TryAssembleProgram_should_outputErrorInvalidInstruction_when_instructionHasUndefinedOpcode(void) {
+void test_TryAssembleProgram_should_outputErrorNoMatchingOverload_when_instructionHasUndefinedMnemonic(void) {
   // Arrange
   initializeProgram("nop");
-  program.lines[0].instruction.opcode = OPCODE_COUNT; // Not a valid opcode
+  program.lines[0].instruction.mnemonic = ASSEMBLY_MNEMONIC_COUNT; // Not a valid mnemonic
 
   TextSpan expectedErrorSpan = {{0, 0}, {0, 3}};
 
@@ -224,7 +181,7 @@ void test_TryAssembleProgram_should_outputErrorInvalidInstruction_when_instructi
 
   // Assert
   TEST_ASSERT_FALSE(success);
-  TEST_ASSERT_EQUAL_STRING(INVALID_INSTRUCTION, error.message);
+  TEST_ASSERT_EQUAL_STRING(NO_MATCHING_OVERLOAD, error.message);
   TEST_ASSERT_EQUIVALENT_TEXT_SPAN(expectedErrorSpan, error.sourceSpan, sourceText);
 }
 
