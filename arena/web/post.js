a353591aa00a0020fcb1ae2543f5aec92b17dfac
Nathan Walker <nathanwalker1208@gmail.com>
2025-05-31 11:40:38 -0600

    Improve code editor layout

--- arena/web/post.js
+++ arena/web/post.js
@@ -1,11 +1,23 @@
+function _updateProgramOutput(result, outputElement) {
+  if (result.length !== 0) {
+    outputElement.innerHTML = "<b>Error</b><br>" + result;
+    outputElement.classList.remove("success");
+    outputElement.classList.add("error");
+  } else {
+    outputElement.innerHTML = "<b>Success!</b>";
+    outputElement.classList.remove("error");
+    outputElement.classList.add("success");
+  }
+}
+
 onCanvasResize = Module.cwrap("OnCanvasResize", null, ["number", "number"])
 _reloadAssemblyProgramA = Module.cwrap("ReloadAssemblyProgramA", "string", ["string"])
-function reloadAssemblyProgramA(programStr) {
+function reloadAssemblyProgramA(programStr, outputElement) {
   var result = _reloadAssemblyProgramA(programStr);
-  if (result.length !== 0) { window.alert(result); }
+  _updateProgramOutput(result, outputElement);
 }
 _reloadAssemblyProgramB = Module.cwrap("ReloadAssemblyProgramB", "string", ["string"])
-function reloadAssemblyProgramB(programStr) {
+function reloadAssemblyProgramB(programStr, outputElement) {
   var result = _reloadAssemblyProgramB(programStr);
-  if (result.length !== 0) { window.alert(result); }
+  _updateProgramOutput(result, outputElement);
 }
